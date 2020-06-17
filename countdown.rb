b7fb36e65a5bd79be1ebc20d90d01969fd0633ad
MIchael Sutton <hicallmesutton@gmail.com>
2020-06-17 03:45:08 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  while num > 0 
+    num -= 1 
+    puts "#{num} SECONDS(S)!"
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num)
+  while num > 0 
+    puts "#{num} SECONDS(S)!"
+    num -= 1 
+    sleep(1)
+  end
+  puts "HAPPY NEW YEAR!"
 end
